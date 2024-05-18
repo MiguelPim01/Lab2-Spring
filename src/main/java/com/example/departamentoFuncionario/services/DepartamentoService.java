05ed5de75ad591947607b5a7a8eff19a0a312053
MiguelPim01 <miguelmip02@gmail.com>
2024-05-18 17:51:54 -0300

    feat:DepartamentoService adicionado, mas dando erro

---
+++ src/main/java/com/example/departamentoFuncionario/services/DepartamentoService.java
@@ -0,0 +1,11 @@
+package com.example.departamentoFuncionario.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartamentoService {
+    
+    // @Autowired
+    // private DepartamentoRepository departamentoRepository;
+
+}
